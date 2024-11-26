687ba43eb0cf5985ffa6e6dde43e91afd05c22f1
Otávio Lourenço <otavioluissv@gmail.com>
2024-11-26 21:32:10 +0000

    Initial commit

---
+++ ContactForm/ContactFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactFormApp.swift
+//  ContactForm
+//
+//  Created by Otavio Lourenço on 26/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ContactFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContactForm/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ContactForm
+//
+//  Created by Otavio Lourenço on 26/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Form{
+                Text("Hello, world!")
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
