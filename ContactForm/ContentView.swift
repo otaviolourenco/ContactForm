6c619d45b00617381cf9f15bc9e9792ee92536ab
Otávio Lourenço <otavioluissv@gmail.com>
2024-11-27 20:34:10 +0000

    Lesson about Pickers and forms

--- ContactForm/ContentView.swift
+++ ContactForm/ContentView.swift
@@ -8,10 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var emailAddress = ""
+    @State private var phoneNumber = ""
+    @State private var message = "Write your message here..."
+    @State private var subject = "Help"
+    @State private var includeLogs = false
+    
+    @State private var preferredContactMethod = ContactMethod.email
+    
+    enum ContactMethod {
+        case email, phone
+      }
+    
     var body: some View {
         VStack {
-            Form{
-                Text("Hello, world!")
+            NavigationStack{
+                Form{
+                    Section(header: Text("How can we reach you?")){
+                        Picker("Contact method", selection: $preferredContactMethod){
+                            Text("E-mail").tag(ContactMethod.email)
+                            Text("Phone").tag(ContactMethod.phone)
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                        switch preferredContactMethod {
+                        case .email:
+                            TextField("E-mail Address", text: $emailAddress)
+                        case .phone:
+                            TextField("Phone", text: $phoneNumber)
+                        }
+                    }
+                    
+                    Section(header: Text("What can we help you with?")){
+                        Picker("Subject", selection: $subject) {
+                            ForEach(["Help", "Suggestion", "Bug Report"], id: \.self) { subject in
+                                Text(subject)
+                            }
+                        }
+                    }
+                    
+                    Section(header: Text("Briefly explain what's going on.")){
+                        TextEditor(text: $message)
+                    }
+                   
+                    Section(header: Text("This information will be sent anonymously.")){
+                        Toggle("Include Logs", isOn: $includeLogs)
+                    }
+                    
+                    Button("Submit", action: {
+                        print("Submit button tapped")
+                    })
+                }
+                .navigationTitle("Contact us")
             }
         }
     }
